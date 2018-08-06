f18b5a0b65c1331b3d4f64dcd4881b75bbc598f9
Bob <bob@bioinf.com>
2018-08-06 16:17:19 +0200

    [WIP] include tensorflow, protobuf, eigen, tensorflow executable project, tensorflow ADTF filter

--- AADCConfig.cmake
+++ AADCConfig.cmake
@@ -76,7 +76,71 @@ if(Boost_FOUND)
 	# UNIX: do not forget to add the path to LD_LIBRARY_PATH: add /opt/boost/1.66.0/lib to file /etc/ld.so.conf.d/boost.conf
 else(Boost_FOUND)
 	message(FATAL_ERROR "Boost not found." )		
-endif(Boost_FOUND)          
+endif(Boost_FOUND)
+
+#------------------------------------------------------------------	
+#--------lib protobuf------------------------------------------
+#------------------------------------------------------------------	
+
+if(UNIX)
+	set(protobuf_MODULE_COMPATIBLE TRUE)	
+	FIND_PACKAGE(Protobuf 3.5.0 REQUIRED)
+
+	if (Protobuf_FOUND)
+		message(STATUS "Found Protobuf lib with version ${Protobuf_VERSION}, libs are: ${Protobuf_LIBRARIES}; Include Dir is ${Protobuf_INCLUDE_DIRS}")
+	else (Protobuf_FOUND)
+		message(FATAL_ERROR "Protobuf lib not found")
+	endif (Protobuf_FOUND)
+endif(UNIX)
+
+#------------------------------------------------------------------	
+#--------lib eigen------------------------------------------
+#------------------------------------------------------------------	
+
+if(UNIX)	
+	FIND_PACKAGE(Eigen3 3.3.4 REQUIRED PATHS "/opt/eigen/3.3.4")
+
+	if (Eigen3_FOUND)
+		message(STATUS "Found Eigen ${EIGEN3_VERSION_STRING} ; Include Dir is ${EIGEN3_INCLUDE_DIRS}")
+	else (Eigen3_FOUND)
+		message(FATAL_ERROR "Eigen not found")
+	endif (Eigen3_FOUND)
+endif(UNIX)
+
+
+
+#------------------------------------------------------------------	
+#--------lib tensorflow------------------------------------------
+#------------------------------------------------------------------	
+
+if(UNIX)
+	set(TENSORFLOW_DIR "/opt/tensorflow/1.8.0")
+	set(TENSORFLOW_INCLUDE_DIRS "${TENSORFLOW_DIR}/include")
+
+
+	FIND_LIBRARY(TENSORFLOW_LIB NAMES 
+		tensorflow_cc
+		PATHS 
+		"${TENSORFLOW_DIR}/lib"
+		)
+
+	FIND_LIBRARY(TENSORFLOW_FRAMEWORK NAMES 
+		tensorflow_framework
+		PATHS 
+		"${TENSORFLOW_DIR}/lib"
+		)
+
+	set(TENSORFLOW_LIBS "${TENSORFLOW_LIB};${TENSORFLOW_FRAMEWORK}")	
+		
+		
+	if (TENSORFLOW_LIBS)
+		message(STATUS "Found Tensorflow. Tensorflow libs are: ${TENSORFLOW_LIBS}; Include Dir is ${TENSORFLOW_INCLUDE_DIRS}")
+	else (TENSORFLOW_LIBS)
+		message(FATAL_ERROR "Tensorflow lib not found under ${TENSORFLOW_DIR}")
+	endif (TENSORFLOW_LIBS)
+         
+endif(UNIX)
+       
 
 #------------------------------------------------------------------	
 #--------lib pylon------------------------------------------
