adcf0be5bc642c8220b12df5a899935b769c0222
aadc <rechberger.markus@gmail.com>
2018-09-02 13:59:19 +0200

    bugfix forgotten braces

--- src/aadcUser/cpp_11_test/cpp_11_test.cpp
+++ src/aadcUser/cpp_11_test/cpp_11_test.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {   
     double test = 1.0;
-    shared_ptr<double> p_test = make_shared<double> test;
+    shared_ptr<double> p_test = make_shared<double>(test);
     cout << "Test: " << test << " Ptr: "<< *p_test << endl;
     return 0;
 }
