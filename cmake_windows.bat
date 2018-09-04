1a3155a1e7e9e516771857eac8ff7540a57feb0f
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2018-09-04 08:31:00 +0200

    Add an optional "init" command argument to the CMake script that creates the VS2015 project

--- cmake_windows.bat
+++ cmake_windows.bat
@@ -1,8 +1,19 @@
-REM load the VS2015 environment
-REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+IF "%1"=="init" GOTO INIT
+IF NOT "%1"=="init" GOTO BASIC
 
-REM set the location of adtf
-SET ADTF_DIR=C:\adtf\3.3.1
+:INIT
+	REM load the VS2015 environment
+	call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+	
+	GOTO BASIC
 
-REM initialize cmake and create the build
-cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/cl.exe" -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/cl.exe" .
+:BASIC
+	REM set the location of adtf
+	SET ADTF_DIR=C:\adtf\3.3.1
+
+	REM initialize cmake and create the build
+	cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/cl.exe" -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/cl.exe" .
+	
+	GOTO END
+
+:END
