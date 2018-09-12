e31d7becab4d7fa2a841144c3b1e2c0c333bfee9
florian <f.haslhofer@gmx.at>
2018-09-12 11:29:51 +0200

    stanley simulation: angle fixes

--- src/aadcUser/controlTest/virtual_car.cpp
+++ src/aadcUser/controlTest/virtual_car.cpp
@@ -1,5 +1,6 @@
 #include "virtual_car.h"
 #include "virtual_point.h"
+#include "math_utilities.h"
 #include "Eigen/Dense"
 
 #include <stdio.h>
@@ -13,67 +14,60 @@ virtualCar::virtualCar()
     carPosition.h = 0.0;
     carSpeed = 1.0;
 
-    stanleyGain = 0.5;
+    stanleyGain = 0.8;
 }
 
 //Params: position of otpimum point, delta time step
 //returns the new position of the car after dtime
 VirtualPoint virtualCar::updateStep(VirtualPoint vp, double dtime)
 {
- 
- //calc speed
-//calc distance car-virtualpoint
- double distance = (vp.getVector2d()-carPosition.getVector2d()).norm();
-// carSpeed = carSpeed + 1*distance;
-//carSpeed = 0.5;
 
-Vector2d diff = vp.getVector2d() - carPosition.getVector2d();
+    //vector between car and virtualpoint
+    Vector2d diff = vp.getVector2d() - carPosition.getVector2d();
 
-int sign = 1;
-if(atan2(diff(1), diff(0))< carPosition.h){
-    sign = -1;
-}
- //calc new direction
+    //calc sign to steer in direction of road
+    int sign = 1;
+    double diff_heading_abs = wrapTo2Pi(atan2(diff(1), diff(0)));
+    if(diff_heading_abs< wrapTo2Pi(carPosition.h)){
+        sign = -1;
+    }
+    //calc new direction
 
- //get optimum point (only for direction-control)
- VirtualPoint directionVirtualPoint = vp; //getOptimumPosition(carPosition);
- //calc normal distance of tangent to car (e)
+    //calc normal distance of tangent to car (e)
+    e = (vp.getVector2d() - carPosition.getVector2d()).norm() * sign;
 
-  e = (directionVirtualPoint.getVector2d() - carPosition.getVector2d()).norm() * sign;
- //calc angle difference between point-tangent and car
- std::cout << "-----------------------" << std::endl;
- std::cout << "point heading : " << directionVirtualPoint.h << std::endl;
- std::cout << "car heading: " << carPosition.h << std::endl;
-  theta_c =  directionVirtualPoint.h - carPosition.h  ;
-std::cout << "Theta_C: " << theta_c << std::endl;
-std::cout << "-----------------------" << std::endl;
+    //calc angle between car heading and point tangent
+    theta_c =  wrapTo2Pi(vp.h) - wrapTo2Pi(carPosition.h);
 
- //calc steering-angle with stanley-approach
- carSteeringAngle = theta_c + atan2(stanleyGain*e, carSpeed);
+    //calc steering-angle with stanley-approach
+    carSteeringAngle = theta_c + atan2(stanleyGain*e, carSpeed);
 
- //estimate next point of the car.
- //get point on front axis
- //Vector2d axisDist(CAR_AXIS_DIST_MM*1000, 0); //axis distance in x axis
- //Rotation2Dd rot(carPosition.h);
- //Vector2d frontAxis =  rot * axisDist;
+    //Debug Messages
+    std::cout << "-----------------------" << std::endl;
+    std::cout << "point heading : " << vp.h << std::endl;
+    std::cout << "car heading: " << carPosition.h << std::endl;
+    std::cout << "diff heading: " << diff_heading_abs << std::endl;
+    std::cout << "e: " << e << std::endl;
+    std::cout << "Theta_C: " << theta_c << std::endl;
+    std::cout << "-----------------------" << std::endl;
 
 
-for (int i=0; i<SIM_STEPS; i++){
- 
-     std::cout << "h: " << carPosition.h << std::endl;
-     carPosition.h += tan(carSteeringAngle)/CAR_AXIS_DIST * carSpeed * dtime/SIM_STEPS;
-     carPosition.x += cos(carPosition.h) * carSpeed * dtime/SIM_STEPS;
-     carPosition.y += sin(carPosition.h) * carSpeed * dtime/SIM_STEPS;
- }
+    for (int i=0; i<SIM_STEPS; i++){
+    
+        std::cout << "h: " << carPosition.h << std::endl;
+        carPosition.h += tan(carSteeringAngle)/CAR_AXIS_DIST * carSpeed * dtime/SIM_STEPS;
+        carPosition.x += cos(carPosition.h) * carSpeed * dtime/SIM_STEPS;
+        carPosition.y += sin(carPosition.h) * carSpeed * dtime/SIM_STEPS;
+    }
 
 
- //calc next point of front axis
- VirtualPoint newFrontAxisPoint;
- //newFrontAxisPoint.x = carPosition.x+ cos(theta_c) * carSpeed  * dtime;
- //newFrontAxisPoint.y = carPosition.y + sin(theta_c) * carSpeed * dtime;
- //newFrontAxisPoint.h = tan(carSteeringAngle) / (CAR_AXIS_DIST);
+    //calc next point of front axis
+    VirtualPoint newFrontAxisPoint;
+    //newFrontAxisPoint.x = carPosition.x+ cos(theta_c) * carSpeed  * dtime;
+    //newFrontAxisPoint.y = carPosition.y + sin(theta_c) * carSpeed * dtime;
+    //newFrontAxisPoint.h = tan(carSteeringAngle) / (CAR_AXIS_DIST);
 
- //carPosition = newFrontAxisPoint;
+    //carPosition = newFrontAxisPoint;
 
 
 
