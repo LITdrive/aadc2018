a472ded5d03c3b2151363171253f5b66d6a960e1
Bob <bob@bioinf.com>
2018-09-21 11:50:53 +0200

    Object detection filter skeleton

---
+++ src/aadcUser/LITD_ObjectDetection/YOLOHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aadc on 21.09.18.
+//
+
+#include "YOLOHandler.h"
---
+++ src/aadcUser/LITD_ObjectDetection/YOLOHandler.h
@@ -0,0 +1,14 @@
+//
+// Created by aadc on 21.09.18.
+//
+
+#ifndef AADC_USER_YOLOHANDLER_H
+#define AADC_USER_YOLOHANDLER_H
+
+
+class YOLOHandler {
+
+};
+
+
+#endif //AADC_USER_YOLOHANDLER_H
