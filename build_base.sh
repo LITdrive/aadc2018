6c6d7095b6213c0ec566498167a622795820a6d0
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2018-09-22 22:27:18 +0200

    Fix the issue with missing plugin description after a clean build

--- build_base.sh
+++ build_base.sh
@@ -31,6 +31,9 @@ cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ../src/aadcBase
 echo "Build debug ..."
 cmake --build . --target install -- -j4
 
+## copy plugin description
+find . -type f -name "*.plugindescription" -print0 | xargs -0 cp -t ../_install/linux64/bin/debug/
+
 cd ..
 
 
@@ -52,6 +55,9 @@ cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../src/aadcBase
 
 echo "Build release ..."
 cmake --build . --target install -- -j4
+
+## copy plugin description
+find . -type f -name "*.plugindescription" -print0 | xargs -0 cp -t ../_install/linux64/bin/
 
 cd ..
 
