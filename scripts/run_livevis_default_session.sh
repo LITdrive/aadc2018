5c7df0d50f3abef3e60eda80e4ccfedd4912bd62
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2018-09-30 13:06:29 +0200

    Add scripts to start the configuration editor and directly run ADTF sessions

---
+++ scripts/run_livevis_default_session.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source $(dirname $0)/init_environment.sh
+
+# start configuration editor
+cd /opt/ADTF/3.3.3/bin/
+/opt/ADTF/3.3.3/bin/adtf_launcher -session=${PROJECT_ROOT}/config/LiveVisualization/adtfsessions/default_session.adtfsession -run
+
