309b7d9153d9454d0467ce143f1a5e0383a9bf86
Bob <bob@bioinf.com>
2018-10-01 12:37:05 +0200

    Fix mat to tensor conversion and load right frozen graph definition

--- src/aadcUser/LITD_ObjectDetection/YOLOHandler.cpp
+++ src/aadcUser/LITD_ObjectDetection/YOLOHandler.cpp
@@ -5,7 +5,7 @@
 #include "YOLOHandler.h"
 
 Status YOLOHandler::load_graph() {
-    string graph = "data/frozen_model.pb";
+    string graph = "data/frozen-yolo-tiny-aadc.pb";
     string root_dir = "/home/aadc/AADC/src/aadcUser/LITD_ObjectDetection/";
     string graph_path = tensorflow::io::JoinPath(root_dir, graph);
 
@@ -25,49 +25,46 @@ Status YOLOHandler::load_graph() {
     return Status::OK();
 }
 
+Tensor readTensorFromMat(const Mat &mat) {
+    int height = mat.rows;
+    int width = mat.cols;
+    int depth = mat.channels();
+    Tensor inputTensor(tensorflow::DT_FLOAT, tensorflow::TensorShape({1, height, width, depth}));
+    auto inputTensorMapped = inputTensor.tensor<float, 4>();
+
+    cv::Mat frame;
+    mat.convertTo(frame, CV_8UC3);
+    const tensorflow::uint8* source_data = (tensorflow::uint8*)frame.data;
+
+    for (int y=0; y<height; y++){
+        const tensorflow::uint8* source_row = source_data + (y*width*depth);
+        for (int x=0; x<width; x++){
+            const tensorflow::uint8* source_pixel = source_row + (x*depth);
+
+            const tensorflow::uint8* source_value_blue = source_pixel;
+            const tensorflow::uint8* source_value_green = source_pixel + 1;
+            const tensorflow::uint8* source_value_red = source_pixel + 2;
+
+            inputTensorMapped(0, y, x, 0) = (*source_value_red)/255.;
+            inputTensorMapped(0, y, x, 1) = (*source_value_green)/255.;
+            inputTensorMapped(0, y, x, 2) = (*source_value_blue)/255.;
+        }
+    }
+
+    return inputTensor;
+}
+
 Tensor YOLOHandler::forward_path(Mat camera_image) {
     int height = 448;
     int width = 448;
     Mat image;
     std::vector<Tensor> outputs;
+    Tensor inputTensor;
 
     resize(camera_image, image, Size(height, width));
+    inputTensor = readTensorFromMat(image);
 
-    int depth = image.channels();
-
-    // creating a Tensor for storing the data
-    tensorflow::Tensor input_tensor(tensorflow::DT_FLOAT, tensorflow::TensorShape({1,height,width,depth}));
-    auto input_tensor_mapped = input_tensor.tensor<float, 4>();
-    const float * source_data = (float*) image.data;
-
-    // copying the data into the corresponding tensor
-    float minR = 255;
-    float maxR = 0;
-    for (int y = 0; y < height; ++y) {
-        const float* source_row = source_data + (y * width * depth);
-        for (int x = 0; x < width; ++x) {
-            const float* source_pixel = source_row + (x * depth);
-            const float* source_B = source_pixel + 0;
-            const float* source_G = source_pixel + 1;
-            const float* source_R = source_pixel + 2;
-
-            input_tensor_mapped(0, y, x, 0) = *source_R;
-            input_tensor_mapped(0, y, x, 1) = *source_G;
-            input_tensor_mapped(0, y, x, 2) = *source_B;
-
-            if ( input_tensor_mapped(0, y, x, 0) < minR ) {
-                minR = input_tensor_mapped(0, y, x, 0);
-            }
-
-            if ( input_tensor_mapped(0, y, x, 0) > maxR ) {
-                maxR = input_tensor_mapped(0, y, x, 0);
-            }
-        }
-    }
-
-    LOG(INFO) << "min " << minR << " max " << maxR << std::endl;
-
-    Status run_status = session->Run({{"input", input_tensor}},
+    Status run_status = session->Run({{"input", inputTensor}},
                                         {"output"}, {}, &outputs);
     if (!run_status.ok()) {
         LOG(ERROR) << "Running model failed: " << run_status;
