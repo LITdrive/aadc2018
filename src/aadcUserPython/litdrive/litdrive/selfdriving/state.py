c2ba78bd67a2a4310ab3a069e2e2cba74b642b68
negentrop <philipp.seidl@gmx.at>
2018-10-08 16:30:01 +0200

    some possible methods for the perceppoin

--- src/aadcUserPython/litdrive/litdrive/selfdriving/state.py
+++ src/aadcUserPython/litdrive/litdrive/selfdriving/state.py
@@ -43,3 +43,61 @@ class Perception:
 
     def is_the_cake_a_lie(self):
         return True
+
+    def is_roadworks_ahead(self):
+        return Fals
+
+    def is_in_tunnel(self):
+        return False
+
+    def get_valid_traffic_signs(self): #or just this
+        #TODO for crossingins
+        return False
+
+    def is_zebra_crossing_ahead(self):
+        #TODO
+        return False
+
+    def is_person_at_zebra_crossing(self):
+        #TODO
+        return False
+
+    def is_person_present(self):
+        #check image TODO
+        return False
+
+    def is_traffic_comming_from_right(self):
+        #TODO
+        return False
+
+    def is_traffic_comming_from_left(self):
+        #TODO
+        # oncoming slow traffic
+        return False
+
+    def is_obstacle_in_trajectory(self):
+        # carwidth = 0.3 meters ;)
+        #TODO --> do not forgett to indicate -_> Blinker
+        # should also consider driving backwards
+        return False
+
+    def is_active_emergency_car(self):
+        #TODO --> yielding
+        return False
+
+    def is_at_intersection(self):
+        # never stop at an intersection!!!!!
+        #TODO
+        return False
+
+    def is_vehicle_coming_from_behind(self):
+        #for merging scenario
+        #TODO
+        return False
+
+
+
+
+
+
+
